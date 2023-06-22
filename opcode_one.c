5b46f6946e30e5f412b5ba08e7bba8711c32c292
Ada <obenedicta4@gmail.com>
2023-06-21 21:52:38 -0700

    Stacks, Queues - LIFO, FIFO

--- opcode_one.c
+++ opcode_one.c
@@ -20,13 +20,19 @@ void _push(stack_t **stack, unsigned int line_number)
 	}
 
 	val = atoi(count_args);
-/*	if (val == 0 && count_args[0] != '0')
+	if (val == 0 && count_args[0] != '0')
 	{
                 fprintf(stderr, "L%d: usage push integer", line_number);
                 exit(EXIT_FAILURE);
 	}
-*/
+
 	insert = malloc(sizeof(stack_t));
+	if (insert == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
 		insert->n = val;
 		insert->prev = NULL;
 		insert->next = (*stack);
